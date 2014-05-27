1c7ce4dcd48c1237c66cd0fc0327d91ae25f210f
TimmWagener <wagenertimm@gmail.com>
2014-05-27 14:37:06 +0200

    Documentation: 1. Added setup.py to be able to build on readthedocs

---
+++ setup.py
@@ -0,0 +1,79 @@
+
+
+
+#import
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+
+    #name
+    name="helga_documentation",
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # http://packaging.python.org/en/latest/tutorial.html#version
+    version='1.0.0',
+
+    description="Helga Documentation",
+    long_description="Pipeline repo for our diploma movie with the working title Helga. Contains the script environment for our DCCs like Maya, Houdini, Nuke etc.",
+
+    # The project's main homepage.
+    url='https://github.com/timmwagener/helga',
+
+    # Author details
+    author='Timm Wagener',
+    author_email='wagenertimm@gmail.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    # What does your project relate to?
+    keywords='DCC pipeline Maya Houdini Nuke Filmakademie Timm Wagener',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(),
+
+    # List run-time dependencies here.  These will be installed by pip when your
+    # project is installed.
+    #install_requires = [],
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    #package_data={},
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages.
+    # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    #data_files=[],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    #entry_points={},
+)

5b1df852f7bf78b74ff2d30568933d90343b7476
TimmWagener <wagenertimm@gmail.com>
2014-06-23 06:04:21 +0200

    Nuke: Added photoscan package and auto_key package for Nuke live keying at photoscan sessions

---
+++ nuke/photoscan/__init__.py
@@ -0,0 +1,10 @@
+
+
+"""
+helga.nuke.photoscan package
+=================================
+
+All packages and modules related to Photoscan action and Nuke can be found here.
+
+**Author:** `Timm Wagener <mailto:wagenertimm@gmail.com>`_
+"""

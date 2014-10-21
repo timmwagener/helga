a88cc9edc1a16dffd15283d096720d143c330981
wagenertimm <wagenertimm@gmail.com>
2014-10-21 15:48:02 +0200

    General: 1. Modified .gitignore to add helga/hiero package in the tracking. Hiero: 1. hiero/Python/Startup/helga_versioning.py added. Carl controls the hiero startup environment. Startup path needs to be added to the env. variables.

---
+++ helga/hiero/__init__.py
@@ -0,0 +1,8 @@
+
+"""
+helga.hiero package
+==================
+
+Helga script environment for Hiero. Here you can find scripts, tools and plugins
+used within Hiero.
+"""
---
+++ helga/hiero/carl/__init__.py
@@ -0,0 +1,11 @@
+
+"""
+helga.hiero.carl package
+============================
+
+All packages and modules created by Carl Schroeter can be found here.
+For bugs, questions or help please ask :
+
+**Author:** `Carl Schroeter <mailto:carl.schroeter@filmakademie.de>`_
+**Pipeline Lead:** `Timm Wagener <mailto:wagenertimm@gmail.com>`_
+"""

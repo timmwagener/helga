74b296a8b0e7037ecd6321e1a9158bf2cfc4d77a
wagenertimm <wagenertimm@gmail.com>
2014-12-21 07:14:18 +0100

    Nuke: 1. Added nuke/rendering/threaded_render package. This package offers functionality to render nuke scripts localy and multithreaded. It does so by dividing the frame range into chunks and spawn a separate nuke process to render each chunk. This is a first functional version and requires some refinement in the future. It is not exposed in the Nuke UI yet.

---
+++ helga/nuke/rendering/__init__.py
@@ -0,0 +1,9 @@
+
+"""
+helga.nuke.rendering package
+=====================================
+
+All packages and modules belonging to rendering in Nuke can be found here.
+
+**Author:** `Timm Wagener <mailto:wagenertimm@gmail.com>`_
+"""
---
+++ helga/nuke/rendering/threaded_render/__init__.py
@@ -0,0 +1,12 @@
+
+"""
+Nuke Threaded Render Package
+==========================================
+
+Render the currently selected write node in a threaded way.
+
+-----------------------
+
+*Author:* `Timm Wagener <mailto:wagenertimm@gmail.com>`_
+*Version:* 0.1
+"""
